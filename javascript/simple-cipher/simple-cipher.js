f87dce7701cec378fa3bbac95aa84dc564c8da1e
Bideo Wego <bideowego@gmail.com>
2017-11-25 00:13:24 -0500

    Refactoring

--- javascript/simple-cipher/simple-cipher.js
+++ javascript/simple-cipher/simple-cipher.js
@@ -1,33 +1,27 @@
 
-
+const _aCode = 97;
+const _zCode = _aCode + 25;
 const _alpha = 'abcdefghijklmnopqrstuvwxyz';
+
+const _map = () => {
+  const map = {};
+  for (let i = 0; i <= 25; i++) {
+    const char = String.fromCharCode(_aCode + i);
+    map[char] = i;
+  }
+  return map;
+};
 
 const _key = () => {
   const empty = Array(100);
   return Array.apply(null, empty)
     .map(() => {
-      const random = Math.random * _alpha.length;
-      const index = Math.floor(random);
-      return _alpha.charAt(index);
+      const r = Math.random() * _alpha.length;
+      const f = Math.floor(r);
+      return _alpha[f];
     })
     .join('');
 };
-
-const _code = (key, str, direction) => {
-  return str.split('')
-    .reduce((output, char, i) => {
-      const j = _mod(i, key.length);
-      const a = key.charAt(j)
-      const offset = direction * _alpha.indexOf(a);
-      const k = _alpha.indexOf(char) + offset;
-      const l = _mod(k, _alpha.length);
-      const b = _alpha.charAt(l);
-      output += b;
-      return output;
-    }, "");
-};
-
-const _mod = (n, m) => ((n % m) + m) % m;
 
 
 class Cipher {
@@ -36,14 +30,40 @@ class Cipher {
       throw new Error("Bad key");
     }
     this.key = key;
+    this._map = _map();
   }
 
   encode(str) {
-    return _code(this.key, str, 1);
+    let encoded = '';
+    for (let i = 0; i < str.length; i++) {
+      const strChar = str[i];
+      const keyChar = this.key[i];
+      const strCharCode = strChar.charCodeAt(0);
+      const keyCharCode = keyChar.charCodeAt(0);
+      const shiftAmount = keyCharCode - _aCode;
+      const shiftedCode = strCharCode + shiftAmount > _zCode ?
+        _aCode + (strCharCode + shiftAmount - _zCode) :
+        strCharCode + shiftAmount;
+      const encodedChar = String.fromCharCode(shiftedCode);
+      encoded += encodedChar;
+    }
+    return encoded;
   }
 
   decode(str) {
-    return _code(this.key, str, -1);
+    let decoded = '';
+    for (let i = 0; i < str.length; i++) {
+      const strChar = str[i];
+      const keyChar = this.key[i];
+      const strCharCode = keyChar.charCodeAt(0);
+      const keyCharCode = strChar.charCodeAt(0);
+      const shiftAmount = keyCharCode - _aCode;
+      const shiftedCode = strCharCode - shiftAmount < _aCode ?
+        _zCode - (strCharCode - shiftAmount + _aCode) :
+        strCharCode - shiftAmount;
+      decoded += String.fromCharCode(shiftedCode);
+    }
+    return decoded;
   }
 }
 
